b28e09c791d798060991261b81ef5d62d2bc44fb
Vini Black <viniibl4ck@gmail.com>
2023-07-19 16:16:21 -0300

    conhecendo o jest

---
+++ test/folhaPagamento.test.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+import { somaHorasExtras, calculaDescontos } from '../index';
+
+test('Deve retornar a soma das horas extras', () => {
+  const esperado = 2500;
+  const retornado = somaHorasExtras(2000, 500);
+
+  expect(retornado).toBe(esperado);
+});
+
+test('Deve descontar o valor do salario', () => {
+  const esperado = 2300;
+  const retornado = calculaDescontos(2500, 200);
+
+  expect(retornado).toBe(esperado);
+});
